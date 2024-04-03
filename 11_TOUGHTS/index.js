0441171154256a656af159405eb71db8d91a3f2c
Neon Ventura <123643824+neon-ventura@users.noreply.github.com>
2024-04-02 21:45:12 -0300

    Finalizando estrutura

--- 11_TOUGHTS/index.js
+++ 11_TOUGHTS/index.js
@@ -6,7 +6,59 @@ const FileStore = require('session-file-store')(session)
 const flash = require('express-flash')
 const conn = require('./db/conn')
 
+// template engine
 
+app.engine('handlebars', exphbs.engine())
+app.set('view-engine', 'handlebars')
+
+// receber resposta do body
+
+app.use(
+    express.urlencoded({
+        extended: true
+    })
+)
+
+app.use(express.json())
+
+// session middleware
+
+app.use(
+    session({
+        name: 'session',
+        secret: 'nosso_secret',
+        resave: false,
+        saveUninitialized: false,
+        store: new FileStore({
+            logFn: () => {},
+            path: require('path').join(require('os').tmpdir(), 'sessions')
+        }),
+        cookie: {
+            secure: false,
+            maxAge: 86400000,
+            expires: new Date(Date.now + 86400000),
+            httpOnly: true
+        }
+    })
+)
+
+// flash messages
+
+app.use(flash())
+
+// public path
+
+app.use(express.static('public'))
+
+// set session to res
+
+app.use((req, res, next) => {
+    if (req.session.userId) {
+        res.locals.session = req.session
+    }
+
+    next()
+})
 
 conn.sync()
 .then(() => {
